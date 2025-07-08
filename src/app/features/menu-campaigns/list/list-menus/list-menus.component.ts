b6deae707c69d9c6c381e5a56c9a0048fa963dca
jcafure <cafure.jaime@gmail.com>
2025-07-07 21:49:38 -0400

    Funcionalidade: Listando os menus

--- src/app/features/menu-campaigns/list/list-menus/list-menus.component.ts
+++ src/app/features/menu-campaigns/list/list-menus/list-menus.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RouterModule } from '@angular/router'; 
 import { MenuCampaignDto } from '../../../../models/menu-campaign.dto';
 import { MenuCampaingService } from '../../../../core/services/menu-campaing.service';
 import { ToastrService } from 'ngx-toastr';
 import { FormBuilder, FormGroup } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-list-menus',
   standalone:true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './list-menus.component.html',
 
 })
@@ -29,14 +30,14 @@ export class ListMenusComponent implements OnInit {
               private toastService: ToastrService) {}
 
   ngOnInit() {
+     this.form = this.formBuilder.group({
+      mealType: ['']
+    });
     this.loandMenus();
   }
 
   loandMenus() {
-      this.form = this.formBuilder.group({
-      name: ['']
-  });
-
+      
     this.menuService.findAllMenusDto(this.currentPage, this.pageSize, this.sortField, 
       this.sortDirection, this.nameFilter).subscribe({
         next: (data: any) => {
